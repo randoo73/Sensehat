40d268933023a5e79c191bf7ffdf81aca6a8b3f1
Randy <randoo73@gmail.com>
2020-04-30 18:44:09 -0700

    Added code to Weather

--- Weather.py
+++ Weather.py
@@ -1,3 +1,19 @@
 
 # https://www.instructables.com/id/Weather-Display-With-Sense-Hat/
 
+from sense_hat import SenseHat
+from time import time
+
+sense = SenseHat()
+temp = round(sense.get_temperature()*1.8+32)
+humidity = round(sense.get_humidity())
+pressure = round(sense.get_pressure())
+
+message = 'Temperature is %d F' \
+          'Humidity is %d percent' \
+          'Pressure is %d mbars'%(temp,humidity,pressure) \
+    \
+sense.show_message(message, scroll_speed = (0.08), text_colour= [200, 0, 200], back colour = [0, 0, 200])
+
+sense.clear()
+
