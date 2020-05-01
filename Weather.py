eb1dba8f007332b900ce2a3a1f6725d68d075b54
Randy <randoo73@gmail.com>
2020-04-30 19:13:32 -0700

    Fixed another Typo in Weather.py

--- Weather.py
+++ Weather.py
@@ -9,10 +9,7 @@ temp = round(sense.get_temperature()*1.8+32)
 humidity = round(sense.get_humidity())
 pressure = round(sense.get_pressure())
 
-message = 'Temperature is %d F' \
-          'Humidity is %d percent' \
-          'Pressure is %d mbars'%(temp,humidity,pressure) \
-    \
+message = 'Temperature is %d F - Humidity is %d percent - Pressure is %d mbars'%(temp,humidity,pressure)
 sense.show_message(message, scroll_speed = (0.08), text_colour= [200, 0, 200], back_colour = [0, 0, 200])
 
 sense.clear()
